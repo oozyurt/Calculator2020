d44d69848a1e9e74c1f941a0b6668e6be04feb06
Oguz Ozyurt <prfoguz@gmail.com>
2020-02-05 22:11:06 -0600

    add substract method

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,8 +9,15 @@ public class Calculator {
         return n1 + n2;
     }
 
+    public static double substrac(double n1, double n2) {
+        Calculator.n1 = n1;
+        Calculator.n2 = n2;
+        return n1 - n2;
+    }
+
     public static void main(String[] args) {
-        System.out.println(add(10,20));
+        System.out.println("ADD: "+add(10,20));
+        System.out.println("Subtract: "+substrac(10,20));
     }
 
 }
