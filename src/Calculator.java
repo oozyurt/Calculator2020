90806a988c74b10a7073f3ce17e45284553893a0
oozyurt <44916306+oozyurt@users.noreply.github.com>
2020-02-08 14:19:48 -0600

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
 //add method
     static double n1;
     static double n2;
-
+//Bera
     public static double add(double n1, double n2) {
         Calculator.n1 = n1;
         Calculator.n2 = n2;
