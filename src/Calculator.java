373bd3cbb33af0e122dd73ce1cf1309c0ce428e9
oozyurt <44916306+oozyurt@users.noreply.github.com>
2020-02-08 22:08:02 -0600

    add divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -21,6 +21,11 @@ public class Calculator {
         return n1 * n2;
     }
     
+      public static double divide(double n1, double n2) {
+        Calculator.n1 = n1;
+        Calculator.n2 = n2;
+        return n1 / n2;
+    }
 
     public static void main(String[] args) {
         System.out.println("ADD: "+add(10,20));
